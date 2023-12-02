1297a9e79d4dbaad49efaf16fe8e14b92c206cb1
Nguyễn Trí Cường <tricuong.can@gmail.com>
2023-12-02 12:50:37 +0700

    39. DTO

--- API/Program.cs
+++ API/Program.cs
@@ -10,11 +10,16 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
-app.MapControllers();
+
+
 // Configure the HTTP request pipeline.
-app.UseHttpsRedirection();
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
+app.MapControllers();
+// app.UseHttpsRedirection();
 
 app.Run();
 
