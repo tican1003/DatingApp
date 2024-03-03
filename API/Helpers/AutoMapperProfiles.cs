6679d1744e44a3d97a80338561fb285974201ad2
tican1003 <tricuong.can@gmail.com>
2024-03-03 15:53:55 +0700

    End of section 10

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -12,5 +12,6 @@ public class AutoMapperProfiles : Profile
         CreateMap<AppUser, MemberDto>().ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url))
         .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CaculateAge()));
         CreateMap<Photo, PhotoDto>();
+        CreateMap<MemberUpdateDto, AppUser>();
     }
 }
